a2c73d7193f839beaf074dc673cd2c23adfb6c00
Developer <daniel.her@onja.org>
2022-12-21 17:17:33 +0300

    Upgrade to .NET 6.0

--- Data/Data/Migrations/ReadLaterDataContextModelSnapshot.cs
+++ Data/Data/Migrations/ReadLaterDataContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace Data.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.5")
+                .HasAnnotation("ProductVersion", "6.0.0")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("Entity.Bookmark", b =>
