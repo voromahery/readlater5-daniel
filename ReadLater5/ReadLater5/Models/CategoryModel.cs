dca1de307fa67d97954876fed00d3dc423302cbc
Developer <daniel.her@onja.org>
2022-12-22 15:33:16 +0300

    Create category model

---
+++ ReadLater5/ReadLater5/Models/CategoryModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+using Entity;
+
+namespace ReadLater5.Models
+{
+    public class CategoryViewModel
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
